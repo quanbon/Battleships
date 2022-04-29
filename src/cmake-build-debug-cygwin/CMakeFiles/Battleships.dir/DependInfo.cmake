f0fb949c5d63bb677502f2adc127bba0937d1a5d
harle <hdsalacup@ucdavis.edu>
2022-04-29 14:40:50 -0700

    added namespace

---
+++ src/cmake-build-debug-cygwin/CMakeFiles/Battleships.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/cygdrive/c/Battleships/src/Board.cpp" "CMakeFiles/Battleships.dir/Board.cpp.o" "gcc" "CMakeFiles/Battleships.dir/Board.cpp.o.d"
+  "/cygdrive/c/Battleships/src/Game.cpp" "CMakeFiles/Battleships.dir/Game.cpp.o" "gcc" "CMakeFiles/Battleships.dir/Game.cpp.o.d"
+  "/cygdrive/c/Battleships/src/Input_Validation.cpp" "CMakeFiles/Battleships.dir/Input_Validation.cpp.o" "gcc" "CMakeFiles/Battleships.dir/Input_Validation.cpp.o.d"
+  "/cygdrive/c/Battleships/src/Move.cpp" "CMakeFiles/Battleships.dir/Move.cpp.o" "gcc" "CMakeFiles/Battleships.dir/Move.cpp.o.d"
+  "/cygdrive/c/Battleships/src/Players.cpp" "CMakeFiles/Battleships.dir/Players.cpp.o" "gcc" "CMakeFiles/Battleships.dir/Players.cpp.o.d"
+  "/cygdrive/c/Battleships/src/Ships.cpp" "CMakeFiles/Battleships.dir/Ships.cpp.o" "gcc" "CMakeFiles/Battleships.dir/Ships.cpp.o.d"
+  "/cygdrive/c/Battleships/src/main.cpp" "CMakeFiles/Battleships.dir/main.cpp.o" "gcc" "CMakeFiles/Battleships.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/cmake-build-debug-cygwin/CMakeFiles/Battleships.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "Battleships.exe"
+  "Battleships.pdb"
+  "CMakeFiles/Battleships.dir/Board.cpp.o"
+  "CMakeFiles/Battleships.dir/Board.cpp.o.d"
+  "CMakeFiles/Battleships.dir/Game.cpp.o"
+  "CMakeFiles/Battleships.dir/Game.cpp.o.d"
+  "CMakeFiles/Battleships.dir/Input_Validation.cpp.o"
+  "CMakeFiles/Battleships.dir/Input_Validation.cpp.o.d"
+  "CMakeFiles/Battleships.dir/Move.cpp.o"
+  "CMakeFiles/Battleships.dir/Move.cpp.o.d"
+  "CMakeFiles/Battleships.dir/Players.cpp.o"
+  "CMakeFiles/Battleships.dir/Players.cpp.o.d"
+  "CMakeFiles/Battleships.dir/Ships.cpp.o"
+  "CMakeFiles/Battleships.dir/Ships.cpp.o.d"
+  "CMakeFiles/Battleships.dir/main.cpp.o"
+  "CMakeFiles/Battleships.dir/main.cpp.o.d"
+  "libBattleships.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Battleships.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
