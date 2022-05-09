7c748b3e7ae845827c649a6d0ec9072c61a45376
harle <hdsalacup@ucdavis.edu>
2022-05-09 13:22:38 -0700

    fixed some rand bs

--- src/Random.h
+++ src/Random.h
@@ -4,5 +4,9 @@
 
 #ifndef BATTLESHIP_RANDOM_H
 #define BATTLESHIP_RANDOM_H
+#include <random>
+
+int rand_orientation (std::mt19937);
+int rand_row_or_col (std::mt19937);
 
 #endif //BATTLESHIP_RANDOM_H
--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,13 @@ int main(int argc, char* argv[]) {
 
     std::ifstream configuration_file;
     configuration_file.open(argv[1]);
-    if (argc > 1) {
+    if (argc == 3) { //OPTIONAL
         int seed = 0;
         sscanf(argv[2], "%d" , seed);
         std::mt19937 rand_gen(seed);
-    } else {
+    } else { // NO SEED, USE TIME
         int time_seed = std::chrono::system_clock::now().time_since_epoch().count();
         std::mt19937 rand_gen(time_seed);
-        // use it by doing rand_gen();
     }
 
     battleship_game.configure_game(configuration_file);
